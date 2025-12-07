968d64aeac1a7bb438b10f1631ee3c36f6e46cad
Jessica C <jcake2@gmail.com>
2025-12-06 19:41:27 -0600

    Smarter solution

--- adventOfCode/day1.py
+++ adventOfCode/day1.py
@@ -1,21 +1,34 @@
+import math
+
+
 def password(inputStr: str, part2: bool = False):
     commands = inputStr.splitlines()
     zero_count = 0
     loc = 50
     for command in commands:
+        starting_loc = loc
         direction = command[0]
         distance = int(command[1:])
-        # TODO make this less dumb... my math was not working
-        for i in range(0, distance):
-            if direction == 'L':
-                loc -= 1
-
-            if direction == 'R':
-                loc += 1
-
-            loc = loc % 100
-            if part2 and loc == 0:
-                zero_count += 1
+        if direction == 'L':
+            loc -= distance
+            if part2 and distance > 0:
+                if loc == 0:
+                    zero_count += 1
+                elif loc < 0:
+                    if starting_loc == 0:
+                        zero_count += math.floor(distance / 100)
+                    else:
+                        zero_count += (math.floor(loc / -100) + 1)
+        if direction == 'R':
+            loc += distance
+            if part2 and distance > 0:
+                if loc == 100:
+                    zero_count += 1
+                elif loc > 100:
+                    if starting_loc == 0:
+                        zero_count += math.floor(distance / 100)
+                    else:
+                        zero_count += math.floor(loc / 100)
 
         loc = loc % 100
         if not part2 and loc == 0:
