a06090a484b044b49207e2af79af1421426fb2c5
Jessica C <jcake2@gmail.com>
2025-12-06 19:45:21 -0600

    Simpler and better

--- adventOfCode/day1.py
+++ adventOfCode/day1.py
@@ -11,24 +11,16 @@ def password(inputStr: str, part2: bool = False):
         distance = int(command[1:])
         if direction == 'L':
             loc -= distance
-            if part2 and distance > 0:
-                if loc == 0:
-                    zero_count += 1
-                elif loc < 0:
-                    if starting_loc == 0:
-                        zero_count += math.floor(distance / 100)
-                    else:
-                        zero_count += (math.floor(loc / -100) + 1)
         if direction == 'R':
             loc += distance
-            if part2 and distance > 0:
-                if loc == 100:
-                    zero_count += 1
-                elif loc > 100:
-                    if starting_loc == 0:
-                        zero_count += math.floor(distance / 100)
-                    else:
-                        zero_count += math.floor(loc / 100)
+
+        if part2 and distance > 0:
+            if starting_loc == 0:
+                zero_count += math.floor(distance / 100)
+            elif direction == 'L':
+                zero_count += (math.floor(loc / -100) + 1)
+            elif direction == 'R':
+                zero_count += math.floor(loc / 100)
 
         loc = loc % 100
         if not part2 and loc == 0:
